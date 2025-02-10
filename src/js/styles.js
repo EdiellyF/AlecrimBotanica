051a19cce51562ea0bbaf1e0adfca42554b700f9
Ediellyf <ediellyferreiradecarvalho@gmail.com>
2025-02-10 10:58:16 -0300

    refactor: enhance plant cards UI and responsiveness

---
+++ src/js/styles.js
@@ -0,0 +1,54 @@
+export function adicionarPlantaPaginaIndex(planta) {
+        
+    const div = document.getElementById("arvores");
+    
+    const plantaDiv = document.createElement('div');
+
+
+    
+        
+        
+    let img = document.createElement('img');
+    img.src = planta.imagem;
+    img.alt = planta.nome;
+   img.loading = "lazy"
+        //Conteiner para as informações da planta
+       const infoDiv = document.createElement('div');
+       infoDiv.classList.add("info");
+
+
+
+        const plantaInfos = [
+            { emoji: "🌿", label: "Nome", value: planta.nome },
+            { emoji: "🌳", label: "Idade", value: planta.idade },
+            { emoji: "📏", label: "Altura", value: planta.altura },
+            { emoji: "⚪", label: "Diâmetro do Tronco", value: planta.diametro_do_tronco },
+            { emoji: "📍", label: "Localização", value: planta.localizacao },
+            { emoji: "🩺", label: "Condição de Saúde", value: planta.condicao_de_saude },
+            { emoji: "✂️", label: "Histórico de Podas", value: planta.historico_de_podas },
+          ];
+
+     
+           plantaInfos.forEach(info => {
+                const p = document.createElement('p');
+                p.classList.add("item");
+                p.innerHTML = `${info.emoji} <strong>${info.label}:</strong> ${info.value}`;
+                div.appendChild(p);
+
+              
+                infoDiv.appendChild(p);
+            }); 
+
+
+        plantaDiv.appendChild(img);
+        plantaDiv.appendChild(infoDiv);
+
+
+        plantaDiv.classList.add("card");
+
+        div.appendChild(plantaDiv);
+          
+ 
+
+
+}
